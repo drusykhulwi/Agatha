e0da6626231ef942cb1cf6d37704e914fb8276ea
Drusilla Khulwael <102147801+drusykhulwi@users.noreply.github.com>
2024-12-27 23:32:15 +0300

    About me section

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='Footer'>
+        <div className='footer-container'></div>
+    </div>
+  )
+}
+
+export default Footer
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
+import About from '../components/About'
 
 function Home() {
   return (
@@ -8,6 +9,7 @@ function Home() {
         <div className='home-container'>
             <Navbar/>
             <Hero/>
+            <About/>
         </div>
     </div>
   )
