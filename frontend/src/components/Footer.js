b30ba7fb75728f90f97b12e56e3b47e138151e92
Drusilla Khulwael <102147801+drusykhulwi@users.noreply.github.com>
2024-12-28 22:36:49 +0300

    Footer section

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,9 +1,43 @@
 import React from 'react'
+import "./Footer.css"
+import Logo from "../logo.png"
+import { Link } from 'react-router-dom'
+import { FaEnvelope, FaLinkedin, FaPhone, FaTwitter } from 'react-icons/fa';
+
 
 function Footer() {
   return (
     <div className='Footer'>
-        <div className='footer-container'></div>
+        <div className='footer-container'>
+           <div className='footer-logo'>
+            <img src={Logo} alt='logo'/>
+            <p>"YOUR MANTRA"</p>
+           </div> 
+           <div className='quick-links'>
+            <h3>QUICK LINKS</h3>
+            <Link to="/">Home</Link>
+            <Link to='/Blog'>Blogs</Link>
+            <Link to='/Podcast'>Podcasts</Link>
+           </div>
+           <div className='contacts'>
+            <h3>CONTACTS</h3>
+            <p>
+                <FaEnvelope/>
+                <a href="mailto:email@gmail.com">email@gmail.com</a>
+            </p>
+            <p>
+                <FaPhone/>
+                <a href="tel:+254712345678">+254712345678</a>
+            </p>
+            <div className='socials'>
+                <a href='https'><FaLinkedin/></a>
+                <a href='https'><FaTwitter/></a>
+            </div>
+           </div>
+        </div>
+        <div className='copyright-container'>
+            Copyright &copy; 2024 <b>Agatha Nafula</b>
+        </div>
     </div>
   )
 }
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
 import About from '../components/About'
+import Footer from '../components/Footer'
 
 function Home() {
   return (
@@ -10,6 +11,7 @@ function Home() {
             <Navbar/>
             <Hero/>
             <About/>
+            <Footer/>
         </div>
     </div>
   )
