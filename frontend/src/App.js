805284a08336cb545838e25222f578509f202d53
Drusilla Khulwael <drusillakhulwael@gmail.com>
2025-05-02 17:44:32 +0300

    Adjusted the app and index files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,17 @@
-// App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home'; // Your Home component
-import Blog from './pages/Blog'; // Your Blog component
-import Podcast from './pages/Podcasts'; // Your Podcast component
+import Homepage from './pages/Home';
+import Blogs from './pages/Blog';
 
 function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Home />} className="route-name"/>
-                <Route path="/Blog" element={<Blog />} className="route-name"/>
-                <Route path="/Podcast" element={<Podcast className="route-name"/>} />
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/blogs" element={<Blogs />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
