c31bc85d03b3dace31c3e5c073dc4ef38c13197d
Drusilla Khulwael <drusillakhulwael@gmail.com>
2025-05-03 21:58:37 +0300

    Adjuted blog page

--- frontend/src/pages/Blog.js
+++ frontend/src/pages/Blog.js
@@ -13,7 +13,7 @@ const Blog = () => {
       <main className="flex-grow container mx-auto px-4 py-8">
         {/* Featured Blog */}
         <div className="relative w-full h-80 md:h-96 mb-8">
-          <img src="/api/placeholder/1200/500" alt="White House" className="w-full h-full object-cover" />
+          <img src="https://images.pexels.com/photos/129112/pexels-photo-129112.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="White House" className="w-full h-full object-cover" />
           <div className="absolute bottom-0 left-0 right-0 p-4 bg-gradient-to-t from-black to-transparent text-white">
             <h2 className="text-xl md:text-2xl font-medium">Blog Title</h2>
             <p className="text-sm opacity-80">Minutes read</p>
@@ -28,7 +28,7 @@ const Blog = () => {
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mb-8">
           {[1, 2, 3].map((blog) => (
             <div key={blog} className="flex flex-col">
-              <img src="/api/placeholder/400/250" alt="Medical professionals" className="w-full h-48 object-cover mb-4" />
+              <img src="https://images.pexels.com/photos/7583367/pexels-photo-7583367.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="Medical professionals" className="w-full h-48 object-cover mb-4" />
               <h3 className="text-gray-400 mb-2">Blog Title</h3>
               <p className="text-gray-700 text-sm mb-4">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ut lacus a justo vehicula hendrerit et in est
