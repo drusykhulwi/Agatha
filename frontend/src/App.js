35852ccd851f6fe8462be99de22da0f51868af5a
Drusilla Khulwael <drusillakhulwael@gmail.com>
2025-05-05 16:30:53 +0300

    Added the podcasts path

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Homepage from './pages/Home';
 import Blogs from './pages/Blog';
+import Podcasts from './pages/PodcastPage';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/blogs" element={<Blogs />} />
+        <Route path="/podcasts" element={<Podcasts />} />
       </Routes>
     </Router>
   );
