6edc20db0ed21a0bf92bc6cbac6bb3bb696c9090
Drusilla Khulwael <drusillakhulwael@gmail.com>
2025-10-31 11:15:02 +0300

    Firebase

--- frontend/src/firebase/Config.js
+++ frontend/src/firebase/Config.js
@@ -1,8 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from 'firebase/firestore';
-// TODO: Add SDKs for Firebase products that you want to use
+import { getStorage } from 'firebase/storage';
+import { getAuth } from 'firebase/auth';
+
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
@@ -19,7 +20,9 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-const analytics = getAnalytics(app);
 
-export { db, analytics };
+const db = getFirestore(app);
+const storage = getStorage(app);
+const auth = getAuth(app);
+
+export { db, storage,  auth };
